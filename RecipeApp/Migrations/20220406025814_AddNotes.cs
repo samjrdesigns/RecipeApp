d258dca416a588ac6b8e6fbb58681ff5e72ff2e3
Steve Medley <samedjr@gmail.com>
2022-04-05 22:27:22 -0500

    Added database migration to change 'modifications' field to 'notes' and create a many-to-many relationship between recipes and ingredients.

---
+++ RecipeApp/Migrations/20220406025814_AddNotes.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeApp.Migrations
+{
+    public partial class AddNotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredient_Recipes_RecipeId",
+                table: "Ingredient");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ingredient_RecipeId",
+                table: "Ingredient");
+
+            migrationBuilder.DropColumn(
+                name: "Modifications",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "Ingredient");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Recipes",
+                type: "varchar(4000)",
+                maxLength: 4000,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "IngredientRecipe",
+                columns: table => new
+                {
+                    IngredientsId = table.Column<int>(type: "int", nullable: false),
+                    RecipesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientRecipe", x => new { x.IngredientsId, x.RecipesId });
+                    table.ForeignKey(
+                        name: "FK_IngredientRecipe_Ingredient_IngredientsId",
+                        column: x => x.IngredientsId,
+                        principalTable: "Ingredient",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientRecipe_Recipes_RecipesId",
+                        column: x => x.RecipesId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientRecipe_RecipesId",
+                table: "IngredientRecipe",
+                column: "RecipesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientRecipe");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Recipes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Modifications",
+                table: "Recipes",
+                type: "varchar(2000)",
+                maxLength: 2000,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RecipeId",
+                table: "Ingredient",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredient_RecipeId",
+                table: "Ingredient",
+                column: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredient_Recipes_RecipeId",
+                table: "Ingredient",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id");
+        }
+    }
+}
